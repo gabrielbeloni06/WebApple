770ce7bc9bb66741933e940d5e05ea23eab1161a
gabriel <gabrielbelonibr@gmail.com>
2025-11-19 15:39:28 -0300

    create: 3d Model

--- code/react/src/App.jsx
+++ code/react/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
+import { Model } from './Model'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#000', overflow: 'hidden' }}>
+      <Canvas dpr={[1, 2]} camera={{ fov: 35, position: [0, 0, 5] }}>
+        <Suspense fallback={null}>
+          <Stage environment="city" intensity={0.6} adjustCamera={1.2}>
+             <Model scale={1} /> 
+          </Stage>
+
+        </Suspense>
+        <OrbitControls autoRotate enableZoom={true} />
+      </Canvas>
+      
+      <div style={{ position: 'absolute', top: 40, left: 40, color: 'white', pointerEvents: 'none' }}>
+        <h1 style={{ fontSize: '4rem', margin: 0 }}>RTX 3090</h1>
+        <p style={{ color: '#aaa' }}>Ultimate Performance</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
-
-export default App
